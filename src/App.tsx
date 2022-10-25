e95fd00f65d19a726ba194aa65d90df3921e3e71
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-10-25 20:31:16 +0200

    change extension of eslint config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React from "react";
+
 function App() {
-  return <p>hello world</p>;
+  return (
+    <div className="container px-4 mx-auto">
+      <p className="text-red-500">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis
+        blanditiis dolore labore veritatis, quae voluptatem beatae nisi fugiat
+        quia esse sunt quos fugit aliquid consequuntur consectetur culpa! Ea,
+        omnis ex.
+      </p>
+    </div>
+  );
 }
 
 export default App;
