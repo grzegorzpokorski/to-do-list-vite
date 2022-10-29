c95e6fedf5b7417c6d4a76812a5870ca6c342ed4
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-10-29 17:35:08 +0200

    remove console.log from useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export const useLocalStorage = <T>(key: string, initialValue: T) => {
-  console.log("run localstorage");
   const [storedValue, setStoredValue] = useState<T>(() => {
     if (typeof window === "undefined") {
       return initialValue;
