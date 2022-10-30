dedaf3fc328828a3486228e5cf591873e94f8663
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-10-30 11:38:58 +0100

    change id of inputs in Task component

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -24,22 +24,22 @@ export const Task = ({
     >
       {task.content}
       <div className="flex flex-row items-center gap-4">
-        <label htmlFor={`${task.createdAt}-'status'`} className="sr-only">
+        <label htmlFor={`${task.createdAt}-status`} className="sr-only">
           oznacz zadanie jako {task.done ? "do zrobienia" : "wykonane"}
         </label>
         <input
           type="checkbox"
           checked={task.done}
           onChange={() => handleChangeTaskStatus(task)}
-          id={`${task.createdAt}-'status'`}
+          id={`${task.createdAt}-status`}
           className="w-4 h-4 text-blue-500 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 cursor-pointer"
         />
-        <label htmlFor={`${task.createdAt}-'detete'`} className="sr-only">
+        <label htmlFor={`${task.createdAt}-detete`} className="sr-only">
           Usuń zadanie
         </label>
         <button
           onClick={() => handleDeleteTask(task)}
-          id={`${task.createdAt}-'detete'`}
+          id={`${task.createdAt}-detete`}
         >
           <FaTrashAlt className="hover:text-red-500 active:text-red-500 transition-colors" />
         </button>
