05b52826e6a80ded1910885cede333063fed5cb7
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-10-30 20:49:17 +0100

    small changes in html markup in form component to improve accessibility

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -31,32 +31,36 @@ export const Form = ({ setPhrase, phrase }: FormProps) => {
   };
 
   return (
-    <form onSubmit={(e) => handleSubmitForm(e)}>
-      <fieldset className="flex justify-between mt-4">
-        <legend className="sr-only">Dodawanie nowego zadania</legend>
-        <input
-          type="text"
-          onChange={(e) => setPhrase(e.target.value)}
-          className="border-2 border-zinc-500 w-full px-2 active:outline-blue-500 focus:outline-blue-500 transition-colors"
-          value={phrase}
-        />
-        {phrase && (
-          <button
-            type="reset"
-            className="px-3 bg-blue-500 hover:bg-blue-600 active:bg-blue-600 text-white cursor-pointer border-white border-l-2 transition-colors"
-            onClick={() => setPhrase("")}
-          >
-            <span className="sr-only">wyczyść pole</span>
-            <MdClear />
-          </button>
-        )}
-        <input
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-600 active:bg-blue-600 disabled:bg-blue-400 text-white cursor-pointer disabled:cursor-not-allowed px-4 py-2 border-white border-l-2 transition-colors"
-          value="Dodaj zadanie"
-          disabled={Boolean(!phrase)}
-        />
-      </fieldset>
+    <form
+      onSubmit={(e) => handleSubmitForm(e)}
+      className="flex justify-between mt-4"
+    >
+      <label htmlFor="form-phrase-input" className="sr-only">
+        treść nowego zadania
+      </label>
+      <input
+        type="text"
+        onChange={(e) => setPhrase(e.target.value)}
+        className="border-2 border-zinc-500 w-full px-2 active:outline-blue-500 focus:outline-blue-500 transition-colors"
+        value={phrase}
+        id="form-phrase-input"
+      />
+      {phrase && (
+        <button
+          type="reset"
+          className="px-3 bg-blue-500 hover:bg-blue-600 active:bg-blue-600 text-white cursor-pointer border-white border-l-2 transition-colors"
+          onClick={() => setPhrase("")}
+        >
+          <span className="sr-only">wyczyść pole</span>
+          <MdClear />
+        </button>
+      )}
+      <input
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 active:bg-blue-600 disabled:bg-blue-400 text-white cursor-pointer disabled:cursor-not-allowed px-4 py-2 border-white border-l-2 transition-colors"
+        value="Dodaj zadanie"
+        disabled={Boolean(!phrase)}
+      />
     </form>
   );
 };
